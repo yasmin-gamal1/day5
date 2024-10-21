24a8aef98947b53f9a2b7cc6d5997c3d937776db
yasmin-gamal1 <yasmingamal.edu0@gmail.com>
2024-10-21 17:15:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int EmployeeId { get; set; }
+
+    public Employee(string name, int employeeId)
+    {
+        Name = name;
+        EmployeeId = employeeId;
+    }
+}
+
+public class Company
+{
+    public event Action<Employee> EmployeeAdded;
+
+    public void AddEmployee(Employee employee)
+    {
+        Console.WriteLine($"Employee {employee.Name} with ID {employee.EmployeeId} has been added to the company.");
+        OnEmployeeAdded(employee);
+    }
+
+    protected virtual void OnEmployeeAdded(Employee employee)
+    {
+        EmployeeAdded?.Invoke(employee);
+    }
+}
+
+public class Club
+{
+    public void AddEmployeeToClub(Employee employee)
+    {
+        Console.WriteLine($"Employee {employee.Name} has been added to the club.");
+    }
+}
+
+public class SocialInsurance
+{
+    public void EnrollEmployeeInInsurance(Employee employee)
+    {
+        Console.WriteLine($"Employee {employee.Name} has been enrolled in social insurance.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var company = new Company();
+        var club = new Club();
+        var socialInsurance = new SocialInsurance();
+
+        company.EmployeeAdded += club.AddEmployeeToClub;
+        company.EmployeeAdded += socialInsurance.EnrollEmployeeInInsurance;
+
+        var newEmployee = new Employee("Ali Mohmmed", 123);
+        company.AddEmployee(newEmployee);
+
+        var anotherEmployee = new Employee("Hagar Ali", 456);
+        company.AddEmployee(anotherEmployee);
+    }
+}
